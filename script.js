04c99b53c34012a8f9ad4c3fb0585d9dc65d6080
Shea Schwenn <sheaschwenn@gmail.com>
2023-03-29 12:33:21 -0700

    random number generator, if statement for tie

--- script.js
+++ script.js
@@ -8,3 +8,20 @@
 // alert who won 
 // depending on outcome input into an array, won, lost, tied increase by one
 // alert play again to take user back to main screen 
+
+var RPS = ["R", "P", "S"];
+var choose = prompt(" Choose R, P, or S");
+var tie = 0;
+var win = 0;
+var lose = 0;
+
+function randomGen(){
+    var randomNum = Math.floor(Math.random() * 3);
+   return RPS[randomNum];
+}
+var computer = randomGen();
+if((choose === "R" && computer === "R") || (choose === "P" && RPS[randomNum] ==="P")|| (choose === "S" && RPS[randomNum] ==="S")){
+    alert("You have tied!");
+    tie = tie+1;
+    alert(tie);
+}
